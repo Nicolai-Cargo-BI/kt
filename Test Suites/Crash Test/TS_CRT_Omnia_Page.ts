ba3ac183b857a82dc30dc892e43933631f9af03a
Nicolai-Cargo-BI <110807771+Nicolai-Cargo-BI@users.noreply.github.com>
2022-09-26 12:45:05 +0200

    ..

---
+++ Test Suites/Crash Test/TS_CRT_Omnia_Page.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS_CRT_Omnia_Page</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>7d9be993-6419-47e8-a511-b8e15b231828</testSuiteGuid>
+   <testCaseLink>
+      <guid>81df5979-6a8b-45b9-ba21-d28ad016ac7e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Crash Test/TC_CRT_Omnia_Page</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
