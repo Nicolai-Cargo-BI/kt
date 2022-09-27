1d3ee0b0bcddac90e08e3453d5217455ebb13380
Nicolai-Cargo-BI <110807771+Nicolai-Cargo-BI@users.noreply.github.com>
2022-09-27 09:49:24 +0200

    ..

---
+++ Test Suites/Crash Test/TS_CRT_ELearning_Page.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS_CRT_ELearning_Page</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>d931fc26-4f32-4a82-9595-33d5bc791440</testSuiteGuid>
+   <testCaseLink>
+      <guid>985c40ad-2fda-4181-b665-6135f2db1bdf</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Crash Test/TC_CRT_ELearning_Page_INPROG</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
